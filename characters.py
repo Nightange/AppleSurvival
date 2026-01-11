11f301f16ecdad50ea1b6e88c2a14dcfad055d5e
Nightange <pyecat7@gmail.com>
2026-01-11 11:45:54 -0500

    put in the introduction to the game and the objects are created, added todo list, modified dragon to remove hunger(will add a concept similar back later, as seen in todo)

--- characters.py
+++ characters.py
@@ -1,8 +1,11 @@
 class apple:
-    def __init__():
-        pass
+    def __init__(self, name, health, hunger):
+        self.name = name
+        self.health = health
+        self.hunger = hunger
     
     
 class dragon:
-    def __init__():
-        pass
+    def __init__(self, health, damage):
+        self.health = health
+        self.damage = damage
--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-def main():
+import characters as c
+
+def intro():
+    print("Welcome to my game! \n You are an apple, in a world full of hungry dragons. You must survive. ")
+    name = input("What would you like to name your apple? ")
+    if name.lower() == "mina":
+        print("Sam loves you! ")
+    print("Hello", name)
+    while True:
+        difficulty = input("Would you like to play on easy, medium, or hard? ").lower()
+        if difficulty == "easy" or difficulty == "medium" or difficulty == "hard":
+            break
+        print("Please choose easy, medium, or hard")
+    
+    
+    difficulty_map = {
+        "easy": {"health": 50, "damage": 20},
+        "medium": {"health": 100, "damage": 35},
+        "hard" : {"health": 200, "damage": 50},
+    }
+    dragon = c.dragon(**difficulty_map[difficulty])
+    user = c.apple(name, 100, 50)
+
+def main_game():
     pass
+    
+
+def main():
+    intro()
+    main_game()
 
 if __name__ == "__main__":
     main()
